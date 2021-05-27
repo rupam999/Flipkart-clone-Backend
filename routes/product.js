0c84056d53c0cc30c7c2b63e0146a55f8707dea8
rupam999 <rupamchakraborty999@gmail.com>
2021-05-27 17:25:54 +0530

    Product Image Upload API added

---
+++ routes/product.js
@@ -0,0 +1,19 @@
+const multer = require('multer');
+const path = require('path');
+const Product = require('../controllers/product'); 
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, '../uploads'))
+    },
+    filename: (req, file, cb) => {
+        const fileName = `${file.fieldname}-${Date.now().toString()}-${file.originalname}`;
+        cb(null, fileName)
+    }
+})
+
+const upload = multer({ storage: storage })
+
+module.exports = (router) => {
+    router.post('/product/add/image', upload.single('productImage'), Product.uploadProductImage);
+}
