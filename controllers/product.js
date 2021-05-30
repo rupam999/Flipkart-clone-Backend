6eacd450fd4c2270aa6cd26888abaaa3eb7a13d2
rupam999 <rupamchakraborty999@gmail.com>
2021-05-30 09:34:32 +0530

    Get Product with Pagination Added

--- controllers/product.js
+++ controllers/product.js
@@ -33,6 +33,47 @@ const addProductDetails = async (req, res) => {
     });
 }
 
+const getAllProducts = async (req, res) => {
+    try {
+        const {search, page=1} = req.query;
+        const pageSize = Number(process.env.PAGINATION_LIMIT);
+        const pageNumber = page;
+        
+        const keyword = search ? {
+            name: {
+                $regex: search,
+                $options: 'i'
+            }
+        } : {};
+
+        const count = await PRODUCT_MODEL.Product.countDocuments({...keyword});
+        const product = await PRODUCT_MODEL.Product.find({...keyword})
+            .limit(pageSize).skip(pageSize * (pageNumber - 1));
+
+        if(product) {
+            res.json({
+                message: 'success',
+                currentPage: Number(page),
+                totalPage: Math.ceil(count / pageSize),
+                product
+            });
+        } else {
+            res.json({
+                message: 'No Product Found'
+            });
+        }
+
+    } catch(error) {
+        console.log('ERROR at find Product', error);
+        res.status(400);
+        res.json({
+            message: 'error',
+            error
+        });
+    }
+}
+
 module.exports = {
-    addProductDetails
+    addProductDetails,
+    getAllProducts
 }
--- routes/product.js
+++ routes/product.js
@@ -2,4 +2,5 @@ const Product = require('../controllers/product');
 
 module.exports = (router) => {
     router.post('/product/add/details', Product.addProductDetails);
+    router.get('/product/find', Product.getAllProducts);
 }
